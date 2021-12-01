262ebbf43833bf75ca10fd055ac54f7749d9fe0f
JDevMen <jdmendoza_90@hotmail.com>
2021-12-01 11:45:47 -0500

    lógica inicial para cargar elementos en la tabla

--- src/components/table.js
+++ src/components/table.js
@@ -1,27 +1,32 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Table from "react-bootstrap/Table";
 import TableBody from "./tableBody";
-class TableComponent extends Component {
-  render() {
-    return (
-      <div id="tabla">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Directed by</th>
-              <th>Country</th>
-              <th>Budget</th>
-              <th>Release</th>
-              <th>Views</th>
-            </tr>
-          </thead>
-          <tbody></tbody>
-        </Table>
-      </div>
-    );
-  }
+function TableComponent() {
+  const [movies, setMovies] = useState([]);
+
+  return (
+    <div id="tabla">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Directed by</th>
+            <th>Country</th>
+            <th>Budget</th>
+            <th>Release</th>
+            <th>Views</th>
+          </tr>
+        </thead>
+        <tbody>
+          {console.log("movies", movies)}
+          {movies.map((e, i) => (
+            <TableBody key={i} movie={e} />
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default TableComponent;
