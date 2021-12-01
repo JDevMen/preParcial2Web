7e86552eecb72c53029f1a919823645b66ca48c1
JDevMen <jdmendoza_90@hotmail.com>
2021-12-01 11:29:03 -0500

    Separación de lógica de la tabla

---
+++ src/components/tableBody.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TableBody = () => {
+  return <tbody></tbody>;
+};
+
+export default TableBody;
---
+++ src/components/tableHead.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TableHead = () => {
+  return (
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Directed by</th>
+        <th>Country</th>
+        <th>Budget</th>
+        <th>Release</th>
+        <th>Views</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHead;
