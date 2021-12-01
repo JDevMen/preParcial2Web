87feed3e6c02498832ff8fbb938a816526128d4b
JDevMen <jdmendoza_90@hotmail.com>
2021-12-01 11:36:00 -0500

    simplificación de lógica de tabla

--- src/components/table.js
+++ src/components/table.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import Table from "react-bootstrap/Table";
-import TableHead from "./tableHead";
 import TableBody from "./tableBody";
 class TableComponent extends Component {
   render() {
     return (
       <div id="tabla">
         <Table striped bordered hover>
-          <TableHead />
-          <TableBody />
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Directed by</th>
+              <th>Country</th>
+              <th>Budget</th>
+              <th>Release</th>
+              <th>Views</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
         </Table>
       </div>
     );
--- src/components/tableBody.js
+++ src/components/tableBody.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const TableBody = () => {
-  return <tbody></tbody>;
+const TableBody = (props) => {
+  return (
+    <tr>
+      <th scope="row">{props.movie.id}</th>
+      <td>{props.movie.name}</td>
+      <td>{props.movie.directedBy}</td>
+      <td>{props.movie.country}</td>
+      <td>{props.movie.budget}</td>
+      <td>{props.movie.releaseDate}</td>
+      <td>{props.movie.views}</td>
+    </tr>
+  );
 };
 
 export default TableBody;
